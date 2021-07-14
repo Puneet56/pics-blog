3f3f09e27fab762f22a40a86936bbfe9c6c03c6a
puneet2508 <puneetdhiman56@gmail.com>
2021-07-14 15:10:22 +0530

    Added general structure and Users List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from 'react-router-dom';
+import NewPlaces from './places/pages/NewPlaces';
+
+import Users from './users/pages/Users';
 
 function App() {
-  return <h1>Let's start!</h1>;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					<Users />
+				</Route>
+				<Route exact path='/places/new'>
+					<NewPlaces />
+				</Route>
+				<Redirect to='/' />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
