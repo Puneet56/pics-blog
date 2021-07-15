ee194e60fb456a3fba4a0b6b3e5578adfb1de324
puneet2508 <puneetdhiman56@gmail.com>
2021-07-15 13:03:20 +0530

    added styling and Main header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import {
-	BrowserRouter as Router,
-	Redirect,
-	Route,
-	Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
 } from 'react-router-dom';
-import NewPlaces from './places/pages/NewPlaces';
 
-import Users from './users/pages/Users';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
-function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path='/'>
-					<Users />
-				</Route>
-				<Route exact path='/places/new'>
-					<NewPlaces />
-				</Route>
-				<Redirect to='/' />
-			</Switch>
-		</Router>
-	);
-}
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
--- src/places/pages/NewPlaces.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NewPlaces = () => {
-	return <h1>New places</h1>;
-};
-
-export default NewPlaces;
